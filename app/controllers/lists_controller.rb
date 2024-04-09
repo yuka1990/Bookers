34143549dcffed90400933821b5195ec571e9e22
YukaTaguchi <164609193+yuka1990@users.noreply.github.com>
2024-04-09 00:50:36 +0000

    [update]almost

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -31,7 +31,7 @@ class ListsController < ApplicationController
   def destroy
     list= List.find(params[:id])
     list.destroy
-    redirect_to '/lists'
+    redirect_to '/lists/new'
   end
   
   private
